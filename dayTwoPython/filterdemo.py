c80f8ba46f794fc7882aef660cd8400427018855
jaymaru333.jm <jaymaru333.jm@gmail.com>
2022-04-25 17:59:31 +0530

    origin-master

---
+++ dayTwoPython/filterdemo.py
@@ -0,0 +1,15 @@
+listx=[34,353,534,344,53,43,356,34,5344,6,34,53,5,35,34,534,534,3,34]
+
+result = filter(lambda x:True if x%2==0 else False,listx)
+print(list(result))
+
+result1 = filter(lambda x:True if x>1000 else False,listx)
+print(list(result1))
+
+
+#filter also needs a callback to be passed
+# the callback has to return boolean and the callback will be called for each
+# each element in the collection if the callback returns true the element
+# would be kept in tact otherwise filter will be called
+# in the result only filtered elements ll be present 
+# if the input list has n elements output list will have <=n elements
--- first.py
+++
@@ -1,7 +0,0 @@
-print("print first python program")
-
-print("enter a number")
-
-a=input()
-
-print(type(a),a)
